6d4575a244ab3918e317948829d39034477da751
Debasish Das <149887980+debasish-dd@users.noreply.github.com>
2026-07-25 20:39:09 +0530

    Time: 2 ms (50.68%) | Memory: 9.8 MB (26.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
         unordered_map<char , int> hash;
+
         for(auto i: s){
             hash[i]++;
         }
         for(auto i: t){
             hash[i]--;
         }
-        string len = s.size()>t.size()?s:t;
-        for(auto i: len){
+        
+        for(auto i: s){
             if(hash[i]!=0){
                 return false;
             }
